fb46b312b38f2af372f7ce9b4069ca38f3781446
Eymen <144282520+EymenHzl@users.noreply.github.com>
2024-02-18 21:20:17 +0300

    Add files via upload

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+
+namespace Business.Concrete
+{
+    //Çıplak class kalmasın
+    public class PersonManager : ApplicantService
+    {
+
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+
+        public List<Person> GetList()
+        {
+            return null;
+
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync(
+                new TCKimlikNoDogrulaRequest(
+                new TCKimlikNoDogrulaRequestBody(person.NationalIndentity,person.FirstName,person.LastName,person.DateOfBirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+        }
+
+
+    }
+}
